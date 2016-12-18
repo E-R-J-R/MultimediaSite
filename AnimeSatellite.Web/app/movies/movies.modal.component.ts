1e157dda7eacc0a17b1091ad601e3bc554d7097e
Erj <Jr@EXIA>
2016-12-18 21:53:40 +0800

    Working Video Embed (Youtube and JWPlayer) Movie Modal format rework Added Underscore.js

--- AnimeSatellite.Web/app/movies/movies.modal.component.ts
+++ AnimeSatellite.Web/app/movies/movies.modal.component.ts
@@ -2,20 +2,27 @@
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { IMovie } from './movie';
 import { IMovieLink } from './movieLink';
+import * as _ from 'underscore';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'movies.modal.component.html'
+    selector: 'movies-modal',
+    templateUrl: 'movies.modal.component.html',
+    styleUrls: ['movies.modal.component.css']
 })
 export class MoviesModalComponent {
 
     @Input() movie: IMovie;
+    
+    movieLink: IMovieLink;
+    
 
     constructor(public activeModal: NgbActiveModal) {}
 
     ngOnInit(): void {
-        
+        this.movieLink = _.first(this.movie.Links);
     } 
+
 
 
 }

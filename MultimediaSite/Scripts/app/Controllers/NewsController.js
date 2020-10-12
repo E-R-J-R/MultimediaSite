e469e0757bfc0da5161587f2f74275dd74228a29
Erj <Jr@EXIA>
2020-10-12 14:12:53 +0800

    Change package json to appropriate site name

--- MultimediaSite/Scripts/app/Controllers/NewsController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var NewsController = (function () {
-    function NewsController() {
-    }
-    return NewsController;
-}());
-exports.NewsController = NewsController;
-//# sourceMappingURL=NewsController.js.map
